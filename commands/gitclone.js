d70cab6fcfd6516b5f8e035c706f292370233f93
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-09-25 09:16:03 +0300

    Update gitclone.js

--- commands/gitclone.js
+++ commands/gitclone.js
@@ -1,15 +1,15 @@
 module.exports = async (context) => {
 
-const { june, m, text } = context;
+const { sock, chatId, message } = context;
 
-if (!text) return m.reply(`Where is the link?`)
-if (!text.includes('github.com')) return m.reply(`Is that a GitHub repo link ?!`)
+if (!text) return chatId.reply(`Where is the link?`)
+if (!text.includes('github.com')) return chatId.reply(`Is that a GitHub repo link ?!`)
 let regex1 = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
-    let [, user3, repo] = text.match(regex1) || []
+    let [, user3, repo] = message.match(regex1) || []
     repo = repo.replace(/.git$/, '')
     let url = `https://api.github.com/repos/${user3}/${repo}/zipball`
     let filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
-    await june.sendMessage(m.chat, { document: { url: url }, fileName: filename+'.zip', mimetype: 'application/zip' }, { quoted: m }).catch((err) => m.reply("error"))
+    await sock.sendMessage(m.chat, { document: { url: url }, fileName: filename+'.zip', mimetype: 'application/zip' }, { quoted: message }).catch((err) => chatId.reply("error"))
 
 }
 
