0335421b308700afe47e2f11ea3ad93897c8f84a
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-09-25 09:07:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ const updateCommand = require('./commands/update');
 const removebgCommand = require('./commands/removebg');
 const { reminiCommand } = require('./commands/remini');
 const getppCommand = require('./commands/getpp');
-const gitcloneCommand = require('./gitclone');
+const gitcloneCommand = require('./commands/gitclone');
 
 // Global settings
 global.packname = settings.packname;
