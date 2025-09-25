dfb151665c4faca14877bf1a460da7b4bf2a757b
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-09-25 08:25:26 +0300

    Update truth.js

--- commands/truth.js
+++ commands/truth.js
@@ -8,12 +8,12 @@ async function truthCommand(sock, chatId, message) {
         if (!res.ok) {
             throw await res.text();
         }
-        const imgxUrl = "https://files.catbox.moe/7ibt7j.jpg";
+       // const imgxUrl = "https://files.catbox.moe/7ibt7j.jpg";
         const json = await res.json();
         const truthMessage = json.result;
 
         // Send the truth message
-        await sock.sendMessage(chatId, { text: truthMessage }, await { image: imgxUrl }, { quoted: message });
+        await sock.sendMessage(chatId, { text: truthMessage }, { quoted: message });
     } catch (error) {
         console.error('Error in truth command:', error);
         await sock.sendMessage(chatId, { text: '❌ Failed to get truth. Please try again later!' }, { quoted: message });
