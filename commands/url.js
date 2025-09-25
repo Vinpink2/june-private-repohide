fddf131757cdf86e55ea746d71e25130c8441aa3
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-09-25 19:05:57 +0300

    Update url.js

--- commands/url.js
+++ commands/url.js
@@ -89,7 +89,7 @@ async function urlCommand(sock, chatId, message) {
             return;
         }
 
-        await sock.sendMessage(chatId, { text: `Your Url: ${url}\n> 🏂June md` }, { quoted: message });
+        await sock.sendMessage(chatId, { text: `Your Url: ${url}\n\n> 🏂June md urls` }, { quoted: message });
     } catch (error) {
         console.error('[URL] error:', error?.message || error);
         await sock.sendMessage(chatId, { text: 'Failed to convert media to URL.' }, { quoted: message });
