aaf377aa09181eb97fd48bcd2e7e703ac5e61f67
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-10-01 01:35:33 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
   author: '‎',
   botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: '', // Your name
-  ownerNumber: '', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'supreme', // Your name
+  ownerNumber: '254798570132', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
