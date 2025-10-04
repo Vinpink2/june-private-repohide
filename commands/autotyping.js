6266270648346619367242c67e18fec10ea4c641
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-10-04 13:57:03 +0300

    Update autotyping.js

--- commands/autotyping.js
+++ commands/autotyping.js
@@ -1,5 +1,5 @@
 /**
- * Knight Bot - A WhatsApp Bot
+ * june md Bot - A WhatsApp Bot
  * Autotyping Command - Shows fake typing status
  */
 
@@ -33,8 +33,8 @@ async function autotypingCommand(sock, chatId, message) {
                     forwardingScore: 1,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD',
+                        newsletterJid: '',
+                        newsletterName: ' MD',
                         serverMessageId: -1
                     }
                 }
@@ -64,7 +64,7 @@ async function autotypingCommand(sock, chatId, message) {
                         forwardingScore: 1,
                         isForwarded: true,
                         forwardedNewsletterMessageInfo: {
-                            newsletterJid: '120363161513685998@newsletter',
+                            newsletterJid: 'ewsletter',
                             newsletterName: 'KnightBot MD',
                             serverMessageId: -1
                         }
@@ -87,7 +87,7 @@ async function autotypingCommand(sock, chatId, message) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
+                    newsletterJid: 'newsletter',
                     newsletterName: 'KnightBot MD',
                     serverMessageId: -1
                 }
@@ -102,7 +102,7 @@ async function autotypingCommand(sock, chatId, message) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
+                    newsletterJid: '@newsletter',
                     newsletterName: 'KnightBot MD',
                     serverMessageId: -1
                 }
