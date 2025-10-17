8c885596b98e6bf053894936937067370b5f9253
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-10-17 19:04:01 +0300

    Update clone.js

--- commands/clone.js
+++ commands/clone.js
@@ -4,19 +4,6 @@ const fetch = require('node-fetch');
 
 
 
-        const userMessage = (
-            message.message?.conversation?.trim() ||
-            message.message?.extendedTextMessage?.text?.trim() ||
-            message.message?.imageMessage?.caption?.trim() ||
-            message.message?.videoMessage?.caption?.trim() ||
-            ''
-        ).toLowerCase().replace(/\.\s+/g, '.').trim();
-const args = userMessage.split(' ').slice(1).join(' ');
-
-
-
-
-
 async function cloneCommand(sock, chatId, message) {
                 try {
                     if (!args[0]) return 
