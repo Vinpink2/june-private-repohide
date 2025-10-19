e70ed842c02c61a9c91025bd4f7711bc44ae84d0
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-10-19 07:55:37 +0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -30,7 +30,7 @@ async function playCommand(sock, chatId, message) {
         const urlYt = video.url;
 
         // Fetch audio data from API
-        const response = await axios.get(`https://api.privatezia.biz.id/api/downloader/ytmp3?url=${urlYt}`);
+        const response = await axios.get(`https://apis-keith.vercel.app/download/mp3?url=${urlYt}`);
         const data = response.data;
 
         if (!data || !data.status || !data.result || !data.result.downloadUrl) {
