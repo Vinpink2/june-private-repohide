debb7b0500cff12a7051ed9243330979debdcbbd
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-10-25 16:48:29 +0300

    Add files via upload

---
+++ commands/github.js
@@ -0,0 +1,60 @@
+const moment = require('moment-timezone');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
+
+async function githubCommand(sock, chatId, message) {
+  try {
+    
+const pushname = message.pushName || "Unknown User";
+    const res = await fetch('https://api.github.com/repos/vinpink2/June-md');
+    if (!res.ok) throw new Error('Error fetching repository data');
+    const json = await res.json();
+
+    let txt = 
+           `🔹  \`𝙹𝚄𝙽𝙴 𝙼𝙳 𝚁𝙴𝙿𝙾 𝙸𝙽𝙵𝙾.\` \n\n`;
+    txt += `🔸  *Name* : ${json.name}\n`;
+    txt += `🔸  *Watchers* : ${json.watchers_count}\n`;
+    txt += `🔸  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
+    txt += `🔸  *Last Updated* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`;
+    txt += `🔸  *REPO* : ${json.html_url}\n\n`;    
+    txt += `🔹  *Forks* : ${json.forks_count}\n`;
+    txt += `🔹  *Stars* : ${json.stargazers_count}\n`;
+    txt += `🔹  *Desc* : ${json.description || 'None'}\n\n`;
+    txt += `@${pushname} _✧ Thank you for choosing June Md Star ⭐ & fork 🔁_`;
+
+    // Use the local asset image
+    const imgPath = path.join(__dirname, '../assets/menu.jpg');
+    const imgBuffer = fs.readFileSync(imgPath);
+
+    /*await sock.sendMessage(chatId, { image: imgBuffer, caption: txt }, { quoted: message });*/
+      
+        await sock.sendMessage(chatId, {
+            image: imgBuffer,
+            caption: txt,
+            contextInfo: {
+                externalAdReply: {
+                    title: 'June Official Repo',
+                    body: "𝐉ᴜɴᴇ 𝐌ᴅ",
+                    mediaType: 1,
+                    sourceUrl: "https://github.com/Vinpink2/june-md",
+                    thumbnailUrl: "https://d.uguu.se/JaOoOUFw.jpg",
+                    renderLargerThumbnail: false,
+                    showAdAttribution: false
+                }
+            }
+        },{ quoted:message});      
+      
+      
+      
+//arect sucess💉
+    await sock.sendMessage(chatId, {
+            react: { text: '✔️', key: message.key }
+        });
+    
+  } catch (error) {
+    await sock.sendMessage(chatId, { text: '❌ Error fetching repository information.' }, { quoted: message });
+  }
+}
+
+module.exports = githubCommand; 
