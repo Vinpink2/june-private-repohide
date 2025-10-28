a0dd7ae095cd2c1fe8e74072e0d80beea505082c
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-10-28 10:23:10 +0300

    Refactor settings to use environment variables
    Updated settings to use environment variables for configuration.

--- settings.js
+++ settings.js
@@ -1,11 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+if (fs.existsSync(path.join(__dirname, './.env'))) {
+    require('dotenv').config({ path: path.join(__dirname, './.env') })
+}
+
+
 const settings = {
-  packname: '',
-  author: '‎',
-  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: 'supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol, just add country code & number without any space
+  packname: process.env.PACK_NAME || '',
+  author: process.env.AUTHOR || '',
+  botName: process.env.BOT_NEME || '',
+  botOwner: Process.env.BOT_OWNER || '', // Your name
+  ownerNumber: process.env.OWNER_NO || , //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: process.env.MODE || '',
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
