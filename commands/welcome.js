86347e91922a62f79b7390134898cc652827e43b
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-11-01 20:00:37 +0300

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 async function welcomeCommand(sock, chatId, message, match) {
     // Check if it's a group
     if (!chatId.endsWith('@g.us')) {
-        await sock.sendMessage(chatId, { text: 'This command can only be used in groups.' });
+        await sock.sendMessage(chatId, { text: 'This command can only be used in groups.' },{ quoted: message });
         return;
     }
 
